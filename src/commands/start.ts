09781ef2c12a11012506c6674ee8dcfd49fb0530
Matthias Kutz <matthias.kutz@viadee.de>
2020-12-27 15:47:18 +0100

    Add configuration loading

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,4 +1,7 @@
 import {Command, flags} from '@oclif/command'
+import * as fs from 'fs-extra'
+import * as path from 'path'
+import Config from '../configuration/config'
 
 export default class Start extends Command {
 
@@ -8,5 +11,10 @@ export default class Start extends Command {
     const startTime = new Date(Date.now())
 
     this.log(`You've started a new Timebox at ${startTime.getHours()}:${startTime.getMinutes()}!`)
+
+    var conf = new Config(this.config.configDir, this.config.home)
+
+    this.log('User config: ' + conf.load())
+    
   }
 }
